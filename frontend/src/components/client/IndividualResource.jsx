36ad2198ba9c1f7b35f707f3146bc146ac5091fe
anusha-c18 <anushac2001@gmail.com>
2023-07-31 12:43:50 +0530

    connected frontend to the backend

---
+++ frontend/src/components/client/IndividualResource.jsx
@@ -0,0 +1,8 @@
+import React from "react";
+import "./IndividualResource.css";
+
+function IndividualResource() {
+  return <div className="individualResource"></div>;
+}
+
+export default IndividualResource;
---
+++ frontend/src/lib/context.jsx
@@ -0,0 +1,23 @@
+import React, { createContext, useEffect, useContext, useState } from "react";
+
+const Context = createContext();
+
+export const StateContext = ({ children }) => {
+  const [availableResources, setAllResources] = useState([]);
+  useEffect(() => {
+    const resources = fetch(
+      "http://localhost:8000/api/routes/records-rt/availableResources",
+      { mode: "cors" },
+      { method: "GET" }
+    )
+      .then((response) => response.json())
+      .then((data) => {
+        setAllResources(data);
+      });
+  }, []);
+  useEffect(() => {
+    console.log(availableResources);
+  }, [availableResources]);
+  return <Context.Provider value={{}}>{children}</Context.Provider>;
+};
+export const useStateContext = () => useContext(Context);
