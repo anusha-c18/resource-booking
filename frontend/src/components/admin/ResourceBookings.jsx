e36bce503b2c431b0cf469b02778d2edc414f66c
anusha-c18 <anushac2001@gmail.com>
2023-08-02 17:51:14 +0530

    feat: accordian content

--- frontend/src/components/admin/ResourceBookings.jsx
+++ frontend/src/components/admin/ResourceBookings.jsx
@@ -1,7 +1,25 @@
 import React from "react";
+import { useStateContext } from "../../lib/context";
 
 function ResourceBookings() {
-  return <div>ResourceBookings</div>;
+  const { allResources } = useStateContext();
+  return (
+    <table className="table">
+      <tr>
+        <th>Start Time</th>
+        <th>End Time</th>
+        <th>Booked</th>
+      </tr>
+      {allResources.map((resource) => (
+        <tr>
+          <td>{resource.startTime}</td>
+          <td>{resource.endTime}</td>
+          <td>{resource.available}</td>
+        </tr>
+      ))}
+      <tr></tr>
+    </table>
+  );
 }
 
 export default ResourceBookings;
