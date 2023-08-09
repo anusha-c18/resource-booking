2cef880bfacc2efa841c6a2ba5dbe5208cb69a88
anusha-c18 <anushac2001@gmail.com>
2023-08-09 16:41:50 +0530

    feat: initialised router

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -9,6 +9,8 @@ import CreateResourceModal from "./components/modals/CreateResourceModal";
 import toast, { Toaster } from "react-hot-toast";
 import DeleteModal from "../src/components/modals/DeleteModal";
 import EditModal from "./components/modals/EditModal";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Login from "./pages/Login";
 
 export const notify = (message) => toast(message);
 
@@ -21,21 +23,26 @@ export function App() {
   } = useStateContext();
   return (
     <>
-      <Toaster />
-      <AnimatePresence>
-        {bookingModalVisibility ? <BookingModal /> : null}
-      </AnimatePresence>
-      <AnimatePresence>
-        {createResourceModalVisibility ? <CreateResourceModal /> : null}
-      </AnimatePresence>
-      <AnimatePresence>
-        {deleteModalVisibility ? <DeleteModal /> : null}
-      </AnimatePresence>
-      <AnimatePresence>
-        {editResourceModal ? <EditModal /> : null}
-      </AnimatePresence>
-      <Client />
-      {/* <Admin /> */}
+      <BrowserRouter>
+        <Routes>
+          {/* <Toaster /> */}
+          {/* <AnimatePresence>
+            {bookingModalVisibility ? <BookingModal /> : null}
+          </AnimatePresence>
+          <AnimatePresence>
+            {createResourceModalVisibility ? <CreateResourceModal /> : null}
+          </AnimatePresence>
+          <AnimatePresence>
+            {deleteModalVisibility ? <DeleteModal /> : null}
+          </AnimatePresence>
+          <AnimatePresence>
+            {editResourceModal ? <EditModal /> : null}
+          </AnimatePresence> */}
+          <Route index element={<Login />} />
+          {/* <Client /> */}
+          {/* <Admin /> */}
+        </Routes>
+      </BrowserRouter>
     </>
   );
 }
