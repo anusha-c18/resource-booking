36c0584995866be56c7d26be1be3169498119c0b
anusha-c18 <anushac2001@gmail.com>
2023-08-18 21:20:56 +0530

    fix: module not found pt 2

---
+++ frontend/src/pages/Admin.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+import ResourceOverview from "../components/admin/Bookings/ResourceOverview";
+import ResourceManagement from "../components/admin/ResourceManagement/ResourceManagement";
+function Admin() {
+  return (
+    <>
+      {/* <ResourceOverview /> */}
+      <ResourceManagement />
+    </>
+  );
+}
+
+export default Admin;
---
+++ frontend/src/pages/Client.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+import AvailableResources from "../components/client/Resources/AvailableResources";
+import MyBookings from "../components/client/Bookings/MyBookings";
+
+function Client() {
+  return (
+    <>
+      {/* <AvailableResources /> */}
+      <MyBookings />
+    </>
+  );
+}
+
+export default Client;
