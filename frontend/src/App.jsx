03b6edcd126603758f690f21deb731abd6ba989f
anusha-c18 <anushac2001@gmail.com>
2023-08-21 19:05:50 +0530

    fix: misconfiguration in parameters 2nd attempt

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -6,12 +6,12 @@ import { AnimatePresence } from "framer-motion";
 import Client from "./pages/Client";
 import Admin from "./pages/Admin";
 import CreateResourceModal from "./components/modals/CreateResourceModal";
+import { Auth0Provider } from "@auth0/auth0-react";
 import toast, { Toaster } from "react-hot-toast";
 import DeleteModal from "../src/components/modals/DeleteModal";
 import EditModal from "./components/modals/EditModal";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Login from "./pages/Login";
-import { useAuth0 } from "@auth0/auth0-react";
 
 export const notify = (message) => toast(message);
 
@@ -23,16 +23,20 @@ export function App() {
     createResourceModalVisibility,
   } = useStateContext();
 
-  const { loginWithPopup, loginWithRedirect, logout, user, isAuthenticated } =
-    useAuth0();
-
   return (
     <>
-      <BrowserRouter>
-        <Routes>
-          <Route index element={<Login />} />
-          {/* <Toaster /> */}
-          {/* <AnimatePresence>
+      <Auth0Provider
+        domain="dev-1k4isffw1z8aw3io.us.auth0.com"
+        clientId="YDATtJWpOnS9ldhuJoIM7FF1d24RXWXf"
+        authorizationParams={{
+          redirect_uri: window.location.origin,
+        }}
+      >
+        <BrowserRouter>
+          <Routes>
+            <Route index element={<Login />} />
+            {/* <Toaster /> */}
+            {/* <AnimatePresence>
             {bookingModalVisibility ? <BookingModal /> : null}
           </AnimatePresence>
           <AnimatePresence>
@@ -44,10 +48,11 @@ export function App() {
           <AnimatePresence>
             {editResourceModal ? <EditModal /> : null}
           </AnimatePresence> */}
-          {/* <Client /> */}
-          {/* <Admin /> */}
-        </Routes>
-      </BrowserRouter>
+            {/* <Client /> */}
+            {/* <Admin /> */}
+          </Routes>
+        </BrowserRouter>
+      </Auth0Provider>
     </>
   );
 }
--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -3,20 +3,11 @@ import ReactDOM from "react-dom/client";
 import { App } from "./App.jsx";
 import "./index.css";
 import { StateContext } from "./lib/context";
-import { Auth0Provider } from "@auth0/auth0-react";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <Auth0Provider
-      domain="dev-1k4isffw1z8aw3io.us.auth0.com"
-      clientID="YDATtJWpOnS9ldhuJoIM7FF1d24RXWXf"
-      authorizationParams={{
-        redirect_uri: window.location.origin,
-      }}
-    >
-      <StateContext>
-        <App />
-      </StateContext>
-    </Auth0Provider>
+    <StateContext>
+      <App />
+    </StateContext>
   </React.StrictMode>
 );
