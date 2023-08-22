b683eb460707bb1551833a47e84e9ba1853c7f13
anusha-c18 <anushac2001@gmail.com>
2023-08-22 12:34:31 +0530

    fix: remove react prefix

--- frontend/src/utils/config.jsx
+++ frontend/src/utils/config.jsx
@@ -1,5 +1,5 @@
-const domain = process.env.REACT_APP_AUTH0_DOMAIN;
-const clientId = process.env.REACT_APP_AUTH0_CLIENT_ID;
+const domain = process.env.AUTH0_DOMAIN;
+const clientId = process.env.AUTH0_CLIENT_ID;
 
 const display = () => {
   console.log(domain, clientId);
