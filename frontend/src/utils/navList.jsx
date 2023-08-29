b49dda721619f5cf4a4d8c3c6f0b2b303c43cc3e
anusha-c18 <anushac2001@gmail.com>
2023-08-29 15:32:41 +0530

    feat: logout

--- frontend/src/utils/navList.jsx
+++ frontend/src/utils/navList.jsx
@@ -1,10 +1,12 @@
 const adminNavItems = [
   { "Resource Management": "/adjmin/resourceManagement" },
   { "Resource Overview": "/admin/resourceOverview" },
+  { Logout: "/" },
 ];
 const clientNavItems = [
   { "My Bookings": "/client/myBookings" },
   { "Available Resources": "/client/availableResources" },
+  { Logout: "/" },
 ];
 
 export { adminNavItems, clientNavItems };
