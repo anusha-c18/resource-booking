52968d3b03db13a5e778f9d344ca242aaa8b805a
anusha-c18 <anushac2001@gmail.com>
2023-08-30 20:34:12 +0530

    fix: collection reference

--- backend/api/controllers/insertBooking.helper.js
+++ backend/api/controllers/insertBooking.helper.js
@@ -7,7 +7,7 @@ async function insertBooking(client, booking) {
     if (result.insertedId) {
       console.log("Document inserted successfully:", result.insertedId);
       let allResources = await db.collection("allResources");
-      const result = await db.allResources.updateOne(
+      const result = await allResources.updateOne(
         {
           resource: booking.resource,
           startTime: booking.startTime,
@@ -15,6 +15,7 @@ async function insertBooking(client, booking) {
         },
         { $set: { available: "0" } }
       );
+      console.log(result);
       if (result.modifiedCount === 1) {
         return true;
       } else {
