f8d4da4459f06cd79602d0c9d1a96707c966b36b
anusha-c18 <anushac2001@gmail.com>
2023-08-30 11:23:03 +0530

    fix: db name

--- backend/api/controllers/createUser.helper.js
+++ backend/api/controllers/createUser.helper.js
@@ -1,7 +1,7 @@
 async function createUser(client, userName, flat) {
   try {
     console.log("in create user");
-    let users = await client.db("resouuceBooking").collection("users");
+    let users = await client.db("resourceBooking").collection("users");
     users.insertOne({ name: userName, flat: flat }, function (err, result) {
       if (err) {
         return false;
