49a7f90f098d0f6f81cdb7586ad66f00e558df79
anusha-c18 <anusha.chaturvedi@hpe.com>
2023-09-15 23:08:17 +0530

    feat: load testing

--- backend/api/routes/records-rt.js
+++ backend/api/routes/records-rt.js
@@ -20,46 +20,36 @@ router.get("/", (req, res) => {
   res.send("we are in records router");
 });
 
-router.route("/allResources").get(checkJwt, controller.getAllResources);
+router.route("/allResources").get(controller.getAllResources);
 
-router.route("/allBookings").get(checkJwt, controller.getAllBookings);
+router.route("/allBookings").get(controller.getAllBookings);
 
-router
-  .route("/availableResources")
-  .get(checkJwt, controller.availableResources);
+router.route("/availableResources").get(controller.availableResources);
 
-router.route("/insertBooking").post(checkJwt, controller.insertBooking);
+router.route("/insertBooking").post(controller.insertBooking);
 
 router
   .route("/uniqueAvailableResources")
-  .get(checkJwt, controller.uniqueAvailableResources);
+  .get(controller.uniqueAvailableResources);
 
-router
-  .route("/uniqueResourcesBooked")
-  .get(checkJwt, controller.uniqueResourcesBooked);
+router.route("/uniqueResourcesBooked").get(controller.uniqueResourcesBooked);
 
 router
   .route("/uniqueExistingResources")
-  .get(checkJwt, controller.uniqueExistingResources);
+  .get(controller.uniqueExistingResources);
 
-router
-  .route("/createNewResource")
-  .post(checkJwt, checkScopes, controller.createNewResource);
+router.route("/createNewResource").post(controller.createNewResource);
 
-router
-  .route("/deleteResource/:resource")
-  .get(checkJwt, checkScopes, controller.deleteResource);
+router.route("/deleteResource/:resource").get(controller.deleteResource);
 
-router
-  .route("/deleteBookings/:resource")
-  .get(checkJwt, checkScopes, controller.deleteBookings);
+router.route("/deleteBookings/:resource").get(controller.deleteBookings);
 
-router.route("/getUserBookings/:name").get(checkJwt, controller.userBookings);
+router.route("/getUserBookings/:name").get(controller.userBookings);
 
-router.route("/checkUser/:name").get(checkJwt, controller.checkUser);
+router.route("/checkUser/:name").get(controller.checkUser);
 
-router.route("/createUser/:name/:flat").get(checkJwt, controller.createUser);
+router.route("/createUser/:name/:flat").get(controller.createUser);
 
-router.route("/getFlat/:name").get(checkJwt, controller.getFlat);
+router.route("/getFlat/:name").get(controller.getFlat);
 
 module.exports = router;
